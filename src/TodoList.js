8c1634ae63dfb619a71fec5afe9b8dc64cbf5ecf
Brian <brian.holland.pr@gmail.com>
2020-08-12 02:16:09 -0700

    styled todolist

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 function TodoList(props) {
-	return <div>{props.todos.map((todo) => <li>{todo.task}</li>)}</div>;
+	return (
+		<Paper>
+			<List>
+				{props.todos.map((todo) => (
+					<Fragment>
+						<ListItem>
+							<ListItemText>{todo.task}</ListItemText>
+						</ListItem>
+						<Divider />
+					</Fragment>
+				))}
+			</List>
+		</Paper>
+	);
 }
 
 export default TodoList;
